86469726b936d702aea246ecfa64413589ab4c2f
Maya <10861407+serenibyss@users.noreply.github.com>
2025-12-04 09:02:18 -0700

    day 4 part 1

---
+++ day4/main.rs
@@ -0,0 +1,78 @@
+use std::{fs, io};
+use std::io::BufRead;
+
+fn main() -> io::Result<()> {
+    let raw = fs::read_to_string("day4/input.txt")?;
+    let grid = to_grid(raw);
+
+    let mut positions: u64 = 0;
+
+    for row in 0..grid.len() {
+        for col in 0..grid[row].len() {
+            if check_position(&grid, row, col) {
+                positions += 1;
+            }
+        }
+    }
+
+    println!("Positions: {}", positions);
+
+    Ok(())
+}
+
+fn to_grid(raw: String) -> Vec<Vec<char>> {
+    raw.lines()
+        .map(|line| line.chars().collect())
+        .collect()
+}
+
+fn check_position(grid: &Vec<Vec<char>>, row: usize, col: usize) -> bool {
+    if grid[row][col] != '@' {
+        return false;
+    }
+
+    let mut papers = 0;
+    const DIRS: [(isize, isize); 8] = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1),  (1, 0),  (1, 1)
+    ];
+
+    for (r, c) in DIRS {
+        let nr = r + row as isize;
+        let nc = c + col as isize;
+
+        if nr < 0 || nc < 0 { continue; }
+        let (nr, nc) = (nr as usize, nc as usize);
+        if nr >= grid.len() || nc >= grid[0].len() { continue; }
+
+        if grid[nr][nc] == '@' {
+            papers += 1;
+        }
+    }
+
+    papers < 4
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_check_position() -> io::Result<()> {
+        let raw = fs::read_to_string("day4/testdata/input_part_1.txt")?;
+        let grid = to_grid(raw);
+        let mut positions: u64 = 0;
+
+        for row in 0..grid.len() {
+            for col in 0..grid[row].len() {
+                if check_position(&grid, row, col) {
+                    positions += 1;
+                }
+            }
+        }
+        assert_eq!(positions, 13);
+        Ok(())
+    }
+}
